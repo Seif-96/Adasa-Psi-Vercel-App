66224a37d5ce1578d18a3c958d9f6ee1b26da279
Seif-96 <seif365.s.seif@gmail.com>
2026-02-02 02:56:53 +0200

    edit

--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.jsx';
 
-import "../node_modules/bootstrap/dist/css/bootstrap.min.css"
-import "../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"
-import "../node_modules/@fortawesome/fontawesome-free/css/all.min.css"
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import '../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js';
+import '../node_modules/@fortawesome/fontawesome-free/css/all.min.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-import { PostsProvider } from './context/Context'
+import { PostsProvider } from './../src/Context/Context.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <PostsProvider>
       <App />
     </PostsProvider>
-  </StrictMode>
-)
+  </StrictMode>,
+);
